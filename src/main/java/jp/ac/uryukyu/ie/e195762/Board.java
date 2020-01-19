ba590af0569106639f195325b09542b53658f44b
yabikuhirohumi <e195762@ie.u-ryukyu.ac.jp>
2020-01-19 20:50:30 +0900

    add PreparedBoard class minute adjust.

--- src/main/java/jp/ac/uryukyu/ie/e195762/Board.java
+++ src/main/java/jp/ac/uryukyu/ie/e195762/Board.java
@@ -1,11 +1,13 @@
 package jp.ac.uryukyu.ie.e195762;
 /**
  * This class makes Board.
- *
+ * int stoneNum[] //石の数を色別に保存。stoneNum[0] = 黒、　stoneNum[1] = 白。
+ * boolean Black; //黒石であるかどうか。true = 黒。
  */
 public class Board {
-    private String list[][] = new String[8][8];
-
+     String list[][] = new String[8][8];
+     private int stoneNum[] = new int[2];
+     private boolean Black;
     //コンストラクタ
     public Board() {
         for (int i1 = 0; i1 < list.length; i1++) {
@@ -13,12 +15,13 @@ public class Board {
                 list[i1][i2] = "*";
             }
         }
-        turnBlack(4,5);
-        turnBlack(5,4);
-        turnWhite(4,4);
-        turnWhite(5,5);
+        turnBlack(4, 5);
+        turnBlack(5, 4);
+        turnWhite(4, 4);
+        turnWhite(5, 5);
+        stoneNum[0] = 2;
+        stoneNum[1] = 2;
     }
-
     /**
      * this method prints board of state.
      */
@@ -43,4 +46,20 @@ public class Board {
     public void turnWhite(int x, int y){
         list[y-1][x-1] = "○";
     }
+
+    /**
+     * 変数Blackの真偽をsetするメソッド
+     * @param BlackOrWhite
+     */
+    public void setIsBlack(String BlackOrWhite){
+        if(BlackOrWhite.equals("Black")){
+            Black = true;
+        }
+        if (BlackOrWhite.equals("White")){
+            Black = false;
+        }
+    }
+
+    public boolean getIsBlack(){return Black;}
+
 }
--- src/main/java/jp/ac/uryukyu/ie/e195762/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e195762/Main.java
@@ -2,8 +2,20 @@ package jp.ac.uryukyu.ie.e195762;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        String inputX,inputY;
         System.out.println("start");
-        Board board = new Board();
-        board.printBoard();
+        Scanner in = new Scanner(System.in);
+        PreparedBoard pb = new PreparedBoard();
+        pb.printBoard();
+        pb.setIsBlack("Black");
+
+        while (pb.gameSet == false){
+            System.out.println("今は"+pb.WhatColor()+"の番です。");
+            System.out.println("x座標を入力してください<<<");
+            inputX = in.nextLine();
+            System.out.println("y座標を入力してください<<<");
+            inputY = in.nextLine();
+            pb.Check();
+        }
     }
 }
